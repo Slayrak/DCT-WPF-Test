d41ff681b19d5cc145547eb02462f32d5f1170e8
Максим Шимків <322topgamerintheworld228@gmail.com>
2023-04-20 17:18:56 +0300

    Add main page layout

---
+++ CryptoApp.Domain/Models/Curency.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoApp.Domain.Models
+{
+    public class Curency
+    {
+        public string CurrencyName { get; set; } = "";
+        public string CurrencyCode { get; set; } = "";
+        public double Price { get; set; }
+        public double Volume { get; set; }
+        public double PriceChange { get; set; }
+
+        public List<string> AvailableMarketsNames { get; set; } = new List<string>();
+
+    }
+}
